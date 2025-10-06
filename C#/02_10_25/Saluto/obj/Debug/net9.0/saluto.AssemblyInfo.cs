3550a6e8f5499691f89b6d56405214ca7a11bbf9
DemonBloodstorm <gabrielefrangiosa8@gmail.com>
2025-10-06 09:44:06 +0200

    .

--- C#/02_10_25/Saluto/obj/Debug/net9.0/saluto.AssemblyInfo.cs
+++ C#/02_10_25/Saluto/obj/Debug/net9.0/saluto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Saluto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f076106f304dfa93c9362e6437414344afeff79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Saluto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Saluto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
