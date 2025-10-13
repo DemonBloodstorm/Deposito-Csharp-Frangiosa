3dd2db3f1e460bdbdb021638564834729009315e
DemonBloodstorm <gabrielefrangiosa8@gmail.com>
2025-10-13 17:59:12 +0200

    Esercizio sigleton facile 2

---
+++ C#/13_10_25/EsercizioSigletonFacile2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+sealed class Logger
+{
+    private List<string> messaggi = new List<string>();
+    private static Logger? _instance;
+
+    private Logger()
+    {
+        Console.WriteLine("Ci sono anche io, il Singleton");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (_instance == null)
+            _instance = new Logger();
+        return _instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+        messaggi.Add(message);
+    }
+
+    public List<string> Messaggi()
+    {
+        return messaggi;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Logger logger = Logger.GetInstance();
+
+        logger.Log("Inizio");
+        Console.WriteLine(" ");
+        logger.Log("Fine");
+
+        foreach (var messaggio in logger.Messaggi())
+            Console.WriteLine(messaggio);
+    }
+}
