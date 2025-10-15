0855eaf47b3cc7e30a589b2d1be4ad103cf7609c
DemonBloodstorm <gabrielefrangiosa8@gmail.com>
2025-10-15 17:24:01 +0200

    Fine esercizio observer facile
    Progetta un'applicazione che simuli un bar. Le bevande di base possono essere decorate con ingredienti
    aggiuntivi come latte, cioccolato, panna, ecc. Ogni decoratore aggiunge un costo e una descrizione alla
    bevanda.

---
+++ C#/15_10_25/EsercizioDecoratorFacile/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IBevanda 
+{
+    string Descrizione();
+    double Costo();
+}
+
+public class Caffe : IBevanda
+{
+    public string Descrizione()
+    {
+        return "Caffè";
+    }
+    public double Costo()
+    {
+        return 1.5;
+    }
+}
+public class Te : IBevanda
+{
+    public string Descrizione()
+    {
+        return "Te'";
+    }
+    public double Costo()
+    {
+        return 2.0;
+    }
+}
+
+public abstract class DecoratoreBevanda : IBevanda //Anche detto barista
+{
+    protected IBevanda _bevanda;
+    protected DecoratoreBevanda(IBevanda bevanda)
+    {
+        _bevanda = bevanda;
+    }
+    public virtual string Descrizione()
+    {
+        return _bevanda.Descrizione();
+    }
+
+    public virtual double Costo()
+    {
+        return _bevanda.Costo();
+    }
+
+} 
+
+class ConLatte : DecoratoreBevanda
+{
+    public ConLatte(IBevanda bevanda) : base(bevanda)
+    {
+    }
+    public override string Descrizione()
+    {
+        if (base.Descrizione().Contains("con latte"))
+        {
+            return base.Descrizione();
+        }
+        return _bevanda.Descrizione() + " con latte";
+    }
+    public override double Costo()
+    {
+        return _bevanda.Costo() + 0.2;
+    }
+}
+
+class ConPanna : DecoratoreBevanda
+{
+    public ConPanna(IBevanda bevanda) : base(bevanda)
+    {
+    }
+    public override string Descrizione()
+    {
+        if (base.Descrizione().Contains("con panna"))
+        {
+            return base.Descrizione();
+        }
+        return base.Descrizione() + " con panna";
+    }
+    public override double Costo()
+    {
+        if (base.Descrizione().Contains("con panna"))
+        {
+            return base.Costo();
+        }
+        return base.Costo() + 0.5;
+    }
+}
+class ConCioccolato : DecoratoreBevanda
+{
+    public ConCioccolato(IBevanda bevanda) : base(bevanda)
+    { 
+    }
+    public override string Descrizione()
+    {
+        if (base.Descrizione().Contains("con cioccolato"))
+        {
+            return base.Descrizione();
+        }
+        return base.Descrizione() + " con cioccolato";
+    }
+    public override double Costo()
+    {
+        if (base.Descrizione().Contains("con cioccolato"))
+        {
+            return base.Costo();
+        }
+        return base.Costo() + 1.0;
+    }
+}
+
+public class MostraMenu{
+    public void SceltaCaffeOTe(){
+        Console.WriteLine($"Quale bevanda vuoi ordinare?");
+        Console.WriteLine($"1. Caffè");
+        Console.WriteLine($"2. Te'");
+    }
+    public void SceltaDecorazione()
+    {
+        Console.WriteLine($"Quale decorazione vuoi aggiungere?");
+        Console.WriteLine($"1. Latte");
+        Console.WriteLine($"2. Panna");
+        Console.WriteLine($"3. Cioccolato");
+    }
+}
+
+public class Program{
+    public static void Main(string[] args)
+    {
+        MostraMenu menu = new MostraMenu();
+        IBevanda bevanda = null;
+        int scelta;
+
+        while (true)
+        {
+            menu.SceltaCaffeOTe();
+            if (int.TryParse(Console.ReadLine(), out scelta))
+            {
+                switch (scelta)
+                {
+                    case 1:
+                        Console.WriteLine("Hai scelto il caffè\n");
+                        bevanda = new Caffe();
+                        break;
+                    case 2:
+                        Console.WriteLine("Hai scelto il te'\n");
+                        bevanda = new Te();
+                        break;
+                    default:
+                        Console.WriteLine("Scelta non valida");
+                        continue;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input non valido.");
+                continue;
+            }
+
+            while (true)
+            {
+                menu.SceltaDecorazione();
+                Console.WriteLine("4. Fine ordine");
+
+                if (int.TryParse(Console.ReadLine(), out scelta))
+                {
+                    if (scelta == 4)
+                    {
+                        break;
+                    }
+
+                    switch (scelta)
+                    {
+                        case 1:
+                            Console.WriteLine("Hai scelto il latte");
+                            bevanda = new ConLatte(bevanda);
+                            break;
+                        case 2:
+                            Console.WriteLine("Hai scelto la panna");
+                            bevanda = new ConPanna(bevanda);
+                            break;
+                        case 3:
+                            Console.WriteLine("Hai scelto il cioccolato");
+                            bevanda = new ConCioccolato(bevanda);
+                            break;
+                        default:
+                            Console.WriteLine("Scelta non valida");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Input non valido.");
+                }
+            }
+
+            Console.WriteLine($"Riepilogo ordine: {bevanda.Descrizione()}");
+            Console.WriteLine($"Costo totale: {bevanda.Costo()}");
+            Console.WriteLine("\nNuovo ordine:");
+        }
+    }
+}
--- C#/15_10_25/EsercizioObserverPomeriggio/Program.cs
+++ C#/15_10_25/EsercizioObserverPomeriggio/Program.cs
@@ -1,2 +1,88 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+interface IObserver
+{
+    void NotificaCreazione(string nomeUtente);
+}
+
+interface ISubject
+{
+    void Aggiungi(IObserver observer);
+    void Rimuovi(IObserver observer);
+    void Notifica(string nomeUtente);
+}
+
+public class gestoreCreazioneUtente : ISubject
+{
+    private List<IObserver> _observers = new List<IObserver>();
+    public void Aggiungi(IObserver observer)
+    {
+        _observers.Add(observer);
+    }
+    public void Rimuovi(IObserver observer)
+    {
+        _observers.Remove(observer);
+    }
+    public void Notifica(string nomeUtente)
+    {
+        foreach(var observer in _observers){
+            observer.NotificaCreazione(nomeUtente);
+        }
+    }
+    public void CreaUtente(string nome)
+    {
+        
+        Notifica(nome);
+    }
+}
+public class Utente
+{
+    public string Nome{get;set;}
+    public override string ToString()
+    {
+        return $"Nome utente: {Nome}";
+    }
+}
+public class UserFactory
+{
+    private static UserFactory? _instanza;
+    private static readonly object _lock = new object();
+    public static UserFactory Istanza{
+        get{
+            if(_instanza == null)
+            lock(_lock){
+                if(_instanza == null)
+                _instanza = new UserFactory();
+            }
+            return _instanza;
+        }
+    }
+    public static Utente Crea(string nome)
+    {
+        return new Utente{Nome = nome};
+    }
+}
+
+public class ModuloLog : IObserver
+{
+    public void NotificaCreazione(string nomeUtente)
+    {
+        Console.WriteLine($"Utente {nomeUtente} creato");
+    }
+}
+public class ModuloMarketing : IObserver
+{
+    public void NotificaCreazione(string nomeUtente)
+    {
+        Console.WriteLine($"Invio email di marketing a {nomeUtente}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
