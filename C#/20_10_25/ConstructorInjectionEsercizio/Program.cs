77bd55a86d3892121cb45fa6402fc429d0127c9d
DemonBloodstorm <gabrielefrangiosa8@gmail.com>
2025-10-20 16:30:42 +0200

    Fine eserczzio Construction Injection 1

---
+++ C#/20_10_25/ConstructorInjectionEsercizio/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public interface IGreeter
+{
+    void Greet(string name);
+}
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"System: {message}");
+    }
+}
+public class ConsoleGreeter : IGreeter
+{
+    public void Greet(string name)
+    {
+        Console.WriteLine($"Hi, I am {name}!");
+    }
+}
+
+public class GreetingService
+{
+    private readonly IGreeter _greeter;
+
+    public GreetingService(IGreeter greeter)
+    {
+        _greeter = greeter;
+    }
+
+    public void Greet(string name)
+    {
+        _greeter.Greet(name);
+    }
+    private readonly ILogger _logger;
+
+    public GreetingService(ILogger logger)
+    {
+        _logger = logger;
+    }
+    public void Log(string message)
+    {
+        _logger.Log(message);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IGreeter greeter = new ConsoleGreeter();
+        GreetingService greetingService = new GreetingService(greeter);
+        greetingService.Greet("Gabriele Frangiosa");
+        ILogger logger = new ConsoleLogger();
+        GreetingService loggerServices = new GreetingService(logger);
+        loggerServices.Log("Hello There!");
+    }
+}
