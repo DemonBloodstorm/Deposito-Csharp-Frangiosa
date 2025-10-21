54cdde03414178966b7d426098c086e7b3ebb369
DemonBloodstorm <gabrielefrangiosa8@gmail.com>
2025-10-21 12:09:14 +0200

    Eliminati errori e Implementa OrderService, servizi associati, PricingStrategy e ProductFactory base

--- C#/21_10_25/EsercizioSetterInjectionMedio/Program.cs
+++ C#/21_10_25/EsercizioSetterInjectionMedio/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 
+#region Interfacce IInventoryService, IPaymentProcessor
 public interface IInventoryService
 {
     void Stock(string[] item);
 }
-public interface IPaaymentProcessor
+#endregion Interfacce IInventoryService, IPaymentProcessor
+
+#region Implemntazione delle interfacce IInventoryService, IPaymentProcessor
+public interface IPaymentProcessor
 {
     void Process(decimal amount);
 }
+public class PaymentProcessor : IPaymentProcessor
+{
+    public void Process(decimal amount)
+    {
+        Console.WriteLine($"Processo pagamento di {amount}");
+    }
+}
+
 public class InventoryService : IInventoryService
 {
     public void Stock(string[] item)
@@ -15,10 +27,46 @@ public class InventoryService : IInventoryService
         Console.WriteLine($"In inventario c'è: {string.Join(", ", item)}");
     }
 }
+#endregion Implemntazione delle interfacce IInventoryService, IPaymentProcessor
+
+#region Interfaccia INotificationSender con implemetazione EmailNotificationSender
+public interface INotificationSender
+{
+    void Send(string message);
+}
+public class EmailNotificationSender : INotificationSender
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Invio email: {message}");
+    }
+}
+#endregion 
+
+#region Interfaccia IPricingStrategy con implementazione PricingStrategy
+public interface IPricingStrategy
+{
+    public decimal CalculatePrice(int quantity, decimal unitPrice);
+    public decimal DiscountPrice(int quantity, decimal unitPrice, decimal discount);
+}
+
+public class PricingStrategy : IPricingStrategy
+{
+    public decimal CalculatePrice(int quantity, decimal unitPrice)
+        => quantity * unitPrice;
+    public decimal DiscountPrice(int quantity, decimal unitPrice, decimal discount)
+    =>  CalculatePrice(quantity, unitPrice) * (1 - discount);
+    
+}
+#endregion
+
+#region Classe OrderService
 public class OrderService
 {
     private IInventoryService _inventoryService;
     private IPaymentProcessor _paymentProcessor;
+    public IPricingStrategy _pricingStrategy { get; set; }
+    public INotificationSender _notificationSender { get; set; }
     public void SetInventoryService(IInventoryService inventoryService)
     {
         _inventoryService = inventoryService;
@@ -32,16 +80,30 @@ public class OrderService
         _inventoryService.Stock(new string[] { item });
         _paymentProcessor.Process(amount);
     }
-}
-
-public interface INotificationSender
-{
-    void Send(string message);
-}
-public class EmailNotificationSender : INotificationSender
-{
-    public void Send(string message)
+    public void NotifyOrder(string item, decimal amount)
     {
-        Console.WriteLine($"Invio email: {message}");
+        _notificationSender.Send($"Ordine per {item} a {amount}");
+    }
+    public decimal CalculatePrice(int quantity, decimal unitPrice)
+    {
+        return _pricingStrategy.CalculatePrice(quantity, unitPrice);
+    }
+    public decimal DiscountPrice(int quantity, decimal unitPrice, decimal discount)
+    {
+        return _pricingStrategy.DiscountPrice(quantity, unitPrice, discount);
     }
 }
+#endregion Classe OrderService
+
+#region ProductFactory
+
+public interface IProduct
+{
+    string Name { get; }
+    decimal Price { get; }
+    void DisplayInfo();
+}
+
+
+
+#endregion ProductFactory
