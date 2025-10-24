58cad6fcf4c2a19030ec0930f096f8f55cf2373e
DemonBloodstorm <gabrielefrangiosa8@gmail.com>
2025-10-24 15:40:39 +0200

    Fine dei tre esercizi

--- C#/24_10_25/EserciziSQL.sql
+++ C#/24_10_25/EserciziSQL.sql
@@ -4,3 +4,13 @@ SELECT DISTINCT Region
 FROM country
 WHERE Continent = 'EUROPE';
 
+/* UTILIZZO DI WHERE E GROUP BY */
+SELECT Name, Population FROM city
+WHERE CountryCode = 'USA' AND Population > 1000000
+ORDER BY Population DESC;
+
+/* UTILIZZO DI GROUP BY CON FUNZIONI DI AGGREGAZIONE */
+SELECT Continent, COUNT(*) AS Numero_Paesi, SUM(Population) AS Popolazione_Totale
+FROM country
+GROUP BY Continent
+ORDER BY Popolazione_Totale DESC;
